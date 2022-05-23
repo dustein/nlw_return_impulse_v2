d5c7cf9c629dc399d900017323c3f0c34d00e4a9
DuStein <steindu@gmail.com>
2022-05-22 21:51:43 -0300

    continuando mobile

---
+++ mobile/src/components/Copyright/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+import { styles } from "./styles";
+
+export function Copyright() {
+  
+  return (
+    < View>
+
+      <Text style={styles.text}>
+        Projeto NLW DuStein 
+      </Text>
+
+    </View>
+  );
+}
---
+++ mobile/src/components/Options/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { View } from "react-native";
+import { Copyright } from '../Copyright'
+import { styles } from "./styles";
+
+export function Copyright() {
+  
+  return (
+    < View style={styles.container}>
+      
+      <Copyright />
+
+    </View>
+  );
+}
